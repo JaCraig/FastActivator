49b27b7e7c520795b6fa9d92601f8da67ccd9a68
James Craig <craig.jamesa@gmail.com>
2022-12-12 21:16:56 -0500

    - Added some benchmarks for things to test. - Update to tests.

---
+++ FastActivator.Benchmarks/Tests/IsVsReferenceEquals.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace FastActivator.Benchmarks.Tests
+{
+    public class IsVsReferenceEquals
+    {
+        private readonly TestClass TestObject = new();
+
+        [Benchmark(Baseline = true)]
+        public void Is() => _ = TestObject is null;
+
+        [Benchmark]
+        public void ReferenceEquals() => _ = TestObject is null;
+
+        private class TestClass
+        { }
+    }
+}
---
+++ FastActivator.Benchmarks/Tests/TypeComparisons.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace FastActivator.Benchmarks.Tests
+{
+    public interface ITestInterface
+    { }
+
+    public class TestClass : ITestInterface
+    { }
+
+    public class TypeComparisons
+    {
+        [Benchmark(Baseline = true)]
+        public void IsAssignableFrom() => typeof(ITestInterface).IsAssignableFrom(typeof(TestClass));
+
+        [Benchmark]
+        public void IsInstanceOfType() => typeof(TestClass).IsInstanceOfType(typeof(ITestInterface));
+    }
+}
