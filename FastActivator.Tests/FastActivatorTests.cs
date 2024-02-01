8a8004e0ee687af95befb83736be68649d2cf4b6
JaCraig <craig.jamesa@gmail.com>
2024-02-01 13:32:32 -0500

    fix: testing merge checks

--- FastActivator.Tests/FastActivatorTests.cs
+++ FastActivator.Tests/FastActivatorTests.cs
@@ -3,6 +3,9 @@ using System;
 
 namespace Fast.Activator.Tests
 {
+    /// <summary>
+    /// Tests for the <see cref="FastActivator"/> class.
+    /// </summary>
     public class FastActivatorTests : TestBaseClass
     {
         protected override Type ObjectType => typeof(FastActivator);
